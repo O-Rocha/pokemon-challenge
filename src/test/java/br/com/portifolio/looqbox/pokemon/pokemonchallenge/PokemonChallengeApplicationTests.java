8e8fc1231227c440e597a206dc2a35e11173e8ea
Gabriel <rocha@pop-os.localdomain>
2022-05-28 07:29:34 -0300

    initial commit

---
+++ src/test/java/br/com/portifolio/looqbox/pokemon/pokemonchallenge/PokemonChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portifolio.looqbox.pokemon.pokemonchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
